4c9fab2a19e89d2607dcd92e273b4a4f5ad67ae3
Marcelo Lara <marcelo@darkblue.tv>
2015-09-04 15:19:57 -0300

    base commit
    AdminLTE template in /Admin (https://almsaeedstudio.com/AdminLTE)
    - implement _layout and related partial views
    - created actions into HomeController
    - implemented MVCSiteMap
    (https://github.com/maartenba/mvcsitemapprovider)
    - bundled base scripts / CDN for common items

---
+++ app/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult tmpWidgets()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        public ActionResult tmpChartsChartsJs()
+        {
+            return View();
+        }
+        public ActionResult tmpChartsMorris()
+        {
+            return View();
+        }
+        public ActionResult tmpChartsFlot()
+        {
+            return View();
+        }
+        public ActionResult tmpChartsInline()
+        {
+            return View();
+        }
+        public ActionResult tmpUIGeneral()
+        {
+            return View();
+        }
+        public ActionResult tmpUIIcons()
+        {
+            return View();
+        }
+        public ActionResult tmpUIButtons()
+        {
+            return View();
+        }
+        public ActionResult tmpUISliders()
+        {
+            return View();
+        }
+        public ActionResult tmpUITimeline()
+        {
+            return View();
+        }
+        public ActionResult tmpUIModals()
+        {
+            return View();
+        }
+        public ActionResult tmpFormsGeneral()
+        {
+            return View();
+        }
+        public ActionResult tmpFormsAdvanced()
+        {
+            return View();
+        }
+        public ActionResult tmpFormsEditor()
+        {
+            return View();
+        }
+        public ActionResult tmpTablesSimple()
+        {
+            return View();
+        }
+        public ActionResult tmpTablesData()
+        {
+            return View();
+        }
+        public ActionResult tmpCalendar()
+        {
+            return View();
+        }
+        public ActionResult tmpMailbox()
+        {
+            return View();
+        }
+
+        public PartialViewResult tmpMailCompose(AjaxOptions ajaxOptions)
+        {
+            return PartialView("_tmpMailCompose");
+        }
+
+        public ActionResult TmpMailboxList()
+        {
+            return PartialView("_tmpMailboxList");
+        }
+    }
+}
---
+++ app/Controllers/SampleDataController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app.Controllers
+{
+    public class SampleDataController : Controller
+    {
+
+        private List<TableData> GetData()
+        {
+
+            var platformList = new List<Platform>
+            {
+                new Platform() {Id = 1, Name = "Windows", Vendor = "Microsoft"},
+                new Platform() {Id = 2, Name = "OSX", Vendor = "Apple"},
+                new Platform() {Id = 3, Name = "Ubuntu", Vendor = "Linux"}
+            };
+            var result = new List<TableData>();
+            result.Add(new TableData() { Id = 1, Browser = "Firefox", RenderingEngine = "Gecko", Platform = platformList.Where(x => x.Id < 3).ToList(), Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 2, Browser = "Seamonkey ", RenderingEngine = "Gecko", Platform = platformList.Where(x => x.Id==2).ToList(), Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 3, Browser = "Safari ", RenderingEngine = "Webkit", Platform = platformList.Where(x => x.Id > 1).ToList(), Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 4, Browser = "PSP browser", RenderingEngine = "Gecko", Platform = platformList.Where(x => x.Id < 3).ToList(), Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 5, Browser = "Opera", RenderingEngine = "Presto", Platform = platformList, Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 6, Browser = "Nokia ", RenderingEngine = "Webkit", Platform = platformList.Where(x => x.Id < 3).ToList(), Grade = "A", Version = "1.8" });
+            result.Add(new TableData() { Id = 7, Browser = "OmniWeb ", RenderingEngine = "Gecko", Platform = platformList.Where(x => x.Id < 3).ToList(), Grade = "A", Version = "1.8" });
+            return result;
+        }
+
+
+        // GET: SampleData
+        public ActionResult Index()
+        {
+            
+
+
+
+            return Json(GetData(), JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class TableData
+    {
+        public int Id { get; set; }
+        public string RenderingEngine { get; set; }
+        public string Browser { get; set; }
+        public List<Platform> Platform { get; set; }
+        public string Version { get; set; }
+        public string Grade { get; set; }
+    }
+
+    public class Platform
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Vendor { get; set; }
+    }
+
+}
